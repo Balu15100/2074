0db29ba35b86e4381a56072af0c313d593577ec1
Balu15100 <balakrishnad15100@gmail.com>
2023-06-09 11:20:27 +0530

    php

---
+++ balu.php
@@ -0,0 +1,19 @@
+<?php
+echo $_POST["first"."last"];
+?>&nbsp;
+<?php
+echo $_POST["last"];
+?><br>
+<?php
+echo $_POST["email"];
+?><br>
+<?php
+ echo $_POST["phone1"];
+ echo $_POST["phone2"];
+ echo $_POST["phone3"];
+// array();
+// $arr=array('phone1','phone2','phone3');
+// echo $_POST["arr"];
+// echo $_POST["msg"];
+// echo $_POST["mySelect"];
+?>
